5c1fe6c590a4e41c2d45faf849b8b8bae07adf9c
Taylor Herron <taylorbherron@gmail.com>
2017-01-10 13:13:04 -0500

    Remove redux logger

--- frontend/middleware/root_middleware.js
+++ frontend/middleware/root_middleware.js
@@ -1,11 +1,7 @@
 import { applyMiddleware } from 'redux';
 import sessionMiddleware from './session_middleware.js';
-import createLogger from 'redux-logger';
 import questionnaireMiddleware from './questionnaire_middleware.js';
 import responseMiddleware from './response_middleware.js';
-
-
-const logger = createLogger();
 
 const rootMiddleware = applyMiddleware(
   sessionMiddleware,
